9c5b4a1df1b53a8771776268cb4975d222284a76
saadisafdar <saadisafdar@outlook.com>
2026-07-19 11:32:36 +0500

    Refactor output files: remove obsolete not-found files, add new build and middleware manifests, and update favicon and index files

--- template-5/next.config.ts
+++ template-5/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
+  output: "export",
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/template-5/out",
+  assetPrefix: "/template-5/out",
 };
 
 export default nextConfig;
